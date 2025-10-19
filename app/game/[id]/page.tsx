276d95824821460efbf0975116ef5c6184536a7f
Timothy Loo <TimothyLoo07@gmail.com>
2025-10-19 02:09:48 +0000

    updated dev hot reload and page

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -1,6 +1,6 @@
 // app/game/[id]/page.tsx
-import Link from "next/link";
-import styles from "./page.module.css";
+import Link from 'next/link';
+import styles from './page.module.css';
 
 interface GamePageProps {
   params: { id: string };
@@ -11,14 +11,14 @@ export function generateStaticParams() {
   return [1, 2, 3, 4, 5].map((id) => ({ id: id.toString() }));
 }
 
-export default function GamePage({ params }: GamePageProps) {
-  const { id } = params;
+export default async function GamePage({ params }: GamePageProps) {
+  const { id } = await params;
 
   return (
     <div className={styles.page}>
       <h1 className={styles.title}>Game {id}</h1>
       <p className={styles.description}>This is the layout for all games.</p>
-      <Link href="/">
+      <Link href='/'>
         <div className={styles.button}>Back to Home</div>
       </Link>
     </div>
